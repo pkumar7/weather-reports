968730804621e5a9d468ef878207886df52ee7bf
pkumar7 <contactprashantat@gmail.com>
2015-01-13 11:54:44 +0530

    adding report related ursl to project urls

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +6,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'weather.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    (r'^report', include('report.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
