1e086f41feea22fe2c889d1e35de8d80f137860d
pkumar7 <contactprashantat@gmail.com>
2015-01-13 11:53:12 +0530

    added views and urls files for django

--- weather/report/views.py
+++ weather/report/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from report.gen_report_data import get_report_data
 
 # Create your views here.
+def view_report(request):
+    report_map = get_report_data()
+    return render(request, 'report.html', report_map)
